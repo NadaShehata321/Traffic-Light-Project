2ed2cae2bf60f71166b25d6f0f43b9dccd5d1813
NadaShehata321 <120189856+NadaShehata321@users.noreply.github.com>
2022-12-09 16:10:06 +0200

    Add files via upload

---
+++ sketch_dec09a.ino.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#line 1 "C:\\Users\\nadan\\Documents\\Arduino\\sketch_dec09a\\sketch_dec09a.ino"
+
+int d=500;
+#line 3 "C:\\Users\\nadan\\Documents\\Arduino\\sketch_dec09a\\sketch_dec09a.ino"
+void setup();
+#line 10 "C:\\Users\\nadan\\Documents\\Arduino\\sketch_dec09a\\sketch_dec09a.ino"
+void loop();
+#line 3 "C:\\Users\\nadan\\Documents\\Arduino\\sketch_dec09a\\sketch_dec09a.ino"
+void setup() {
+
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13,HIGH);
+  delay(d);
+  digitalWrite(13,LOW);
+  digitalWrite(12,HIGH);
+  delay(d/2);
+  digitalWrite(12,LOW);
+  digitalWrite(11,HIGH);
+  delay(d);
+  digitalWrite(11,LOW);
+
+}
+
